9d6d768b53a1c808ff1fd13747ac214a1825415b
Maxim Krivoshein <max.kriv0s@gmail.com>
2026-05-03 02:17:42 +0300

    feat: add zsh aliases and exports, migrate to fnm

---
+++ zsh/.config/zsh/aliases.zsh
@@ -0,0 +1,15 @@
+# ── Dotfiles ─────────────────────────────────────────────────────────────────
+alias dotfiles="cd ~/dotfiles"
+
+# ── Замены стандартных утилит ─────────────────────────────────────────────────
+alias ls="eza --tree --level=1 --icons=always --no-time --no-user --no-permissions"
+alias ll="eza -l --icons=always"
+alias la="eza -la --icons=always"
+alias cat="bat"
+
+# ── Git ──────────────────────────────────────────────────────────────────────
+alias g="git"
+alias gs="git status"
+alias gp="git push"
+alias gl="git pull"
+alias glog="git lg"
--- zsh/.config/zsh/exports.zsh
+++ zsh/.config/zsh/exports.zsh
@@ -1,2 +1,11 @@
 # fnm
 eval "$(fnm env --use-on-cd --shell zsh)"
+
+# If you come from bash you might have to change your $PATH.
+# export PATH=$HOME/bin:$HOME/.local/bin:/usr/local/bin:$PATH
+export PATH=$PATH:~/go/bin
+
+# zsh completions (brew) 
+if type brew &>/dev/null; then
+  FPATH=$(brew --prefix)/share/zsh/site-functions:$FPATH
+fi

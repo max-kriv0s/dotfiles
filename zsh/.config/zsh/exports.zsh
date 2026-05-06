1464be7c205e6f845047ad4cb1a450a2672c5c16
Maxim Krivoshein <max.kriv0s@gmail.com>
2026-05-06 23:32:33 +0300

    feat: modify configs

--- zsh/.config/zsh/exports.zsh
+++ zsh/.config/zsh/exports.zsh
@@ -4,6 +4,7 @@ eval "$(fnm env --use-on-cd --shell zsh)"
 # If you come from bash you might have to change your $PATH.
 # export PATH=$HOME/bin:$HOME/.local/bin:/usr/local/bin:$PATH
 export PATH=$PATH:~/go/bin
+export PATH=$PATH:$HOME/.local/bin
 
 # zsh completions (brew) 
 if type brew &>/dev/null; then
